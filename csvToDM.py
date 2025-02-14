b1b86eccecfc6306a64fff7f42a1b73dc6c54313
iTzOutmind <reddug@gmail.com>
2025-02-14 17:02:10 +0100

    Preparing csv to dmatrix conversion

---
+++ csvToDM.py
@@ -0,0 +1,6 @@
+import numpy as np
+import pandas as pd
+from xgboost import DMatrix, train
+
+heartattack = pd.read_csv('C:/Users/reddu/Documents/GitHub/arduino-xgboost/heart_attack_risk_dataset.csv')
+
