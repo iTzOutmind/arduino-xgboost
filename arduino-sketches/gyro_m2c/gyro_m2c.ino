e823af0b36fdd593ab4b21930daa867aecc65222
Denis Reddig <d.reddig@uni-muenster.de>
2025-02-22 15:35:13 +0100

    Moved to untracked dir

--- arduino-sketches/gyro_m2c/gyro_m2c.ino
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fd6cd0b59e046ee2305eb78491f843eb48cbead156d5b56bd7b910a71bcdd631
-size 1891667
--- arduino-sketches/harus_m2c/harus_m2c.ino
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:16497c689c231b80b831ed60e56b93dc72f01bb85b3dd90278218f5ac1e3e53b
-size 230785
