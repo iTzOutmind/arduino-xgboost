3d22b7d01ac408fbfb72c484a8dd25e9fcb4c67a
Denis Reddig <d.reddig@uni-muenster.de>
2025-02-22 17:33:57 +0100

    Generated csv's are now saved in dir set by param

--- serial_reader.py
+++ serial_reader.py
@@ -1,6 +1,10 @@
 import serial
 
-def readserial(comport, baudrate, csv):
+gyro = '/home/denis/arduino-xgboost/datasets/gyro/'
+har = '/home/denis/arduino-xgboost/datasets/har/'
+harus = '/home/denis/arduino-xgboost/datasets/harus/'
+
+def readserial(comport, baudrate, csv, project = ''):
     ser = serial.Serial(comport, baudrate, timeout=0.1)         # 1/timeout is the frequency at which the port is read
 
     while True:
@@ -8,7 +12,7 @@ def readserial(comport, baudrate, csv):
 
         if data == '##### REPEATING... #####':  # Listen for '##### REPEATING... #####'
             if csv == True:
-                printCSV(ser)
+                printCSV(ser, project)
             else:
                 printData(ser)
             break
@@ -21,8 +25,8 @@ def printData(ser):                             # Prints every Serial Output unt
         if data and data != '##### REPEATING... #####':
             print(data)
 
-def printCSV(ser):
-    with open('inoCapture.csv', 'w') as f:
+def printCSV(ser, project):
+    with open(project + 'inoCapture.csv', 'w') as f:
         data = ser.readline().decode().strip()
         while data != '##### REPEATING... #####':
             data = ser.readline().decode().strip()
@@ -30,3 +34,4 @@ def printCSV(ser):
             if data and data != '##### REPEATING... #####':
                 f.write(data + '\n')
 
+readserial('/dev/ttyACM0', 115200, True, gyro)
