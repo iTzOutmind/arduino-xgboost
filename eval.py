1e61112b8d5a766c2de11ee7e7b0ba950ac0f914
Denis Reddig <d.reddig@uni-muenster.de>
2025-02-23 15:11:36 +0100

    Added SummaryStatistics()-func | Added typings for func-params

--- eval.py
+++ eval.py
@@ -1,7 +1,9 @@
 from sklearn import metrics
 import matplotlib.pyplot as pyplot
+from xgboost import XGBClassifier
+from pandas import DataFrame
 
-def printConfusionMatrix(model, xtest, ytest):
+def ConfusionMatrix(model: XGBClassifier, xtest: DataFrame, ytest: DataFrame):
     """
     Plot the confusion matrix for the given model.
 
@@ -16,7 +18,7 @@ def printConfusionMatrix(model, xtest, ytest):
     metrics.ConfusionMatrixDisplay.from_estimator(model, xtest, ytest, cmap='Blues')
     pyplot.show()
 
-def plotLossCurves(model):          
+def LossCurves(model: XGBClassifier):          
     """
     Plot the loss curves from the model's evaluation results.
 
@@ -37,22 +39,36 @@ def plotLossCurves(model):
     pyplot.legend()
     pyplot.show()
 
-def printClassReport(ytest, yhat, precision=4):
+def ClassReport(ytest: DataFrame, yhat, precision: int=4):
     """
-    Print the classification report for the given test labels and predictions.
+    Print a classification report for the given true and predicted labels.
 
     Parameters:
-    ytest (array-like): The true labels for the test data.
-    yhat (array-like): The predicted labels from the model.
-    precision (int, optional): The number of decimal places to include in the report. Default is 4.
+    ytest (array-like): The true labels.
+    yhat (array-like): The predicted labels.
+    precision (int): The number of decimal places to round each score to.
+                    Default is 4.
 
     Returns:
     None
     """
-
     print(metrics.classification_report(ytest, yhat, digits = precision))
 
-def printMisc(model, bestIter, ytest, yhat):
+def SummaryStatistics(df: DataFrame):
+    """
+    Prints the statistical summary for each column in the given dataframe.
+    
+    Parameters:
+    df (pandas.DataFrame): The dataframe to generate the summary statistics for.
+    
+    Returns:
+    None
+    """
+    dfCols = df.columns.to_list()
+    for col in dfCols:
+        print(f"Column: {col} \n{df[col].describe()} \nData Type: {df[col].dtype}\n")
+
+def printMisc(model: XGBClassifier, bestIter: int, ytest: DataFrame, yhat: DataFrame):
     """
     Print miscellaneous evaluation metrics for the given model.
     Not intended for use!
@@ -71,17 +87,3 @@ def printMisc(model, bestIter, ytest, yhat):
     print(f'Test Accuracy: \t{metrics.accuracy_score(ytest, yhat)}')
     print(f'Base_Score{model.base_score}')
     print(f'\nPredict_Proba Return: \n{model.predict_proba(xtest)}')
-
-def printSummaryStatistics(df):
-    """
-    Prints the statistical summary for each column in the given dataframe.
-    
-    Parameters:
-    df (pandas.DataFrame): The dataframe to generate the summary statistics for.
-    
-    Returns:
-    None
-    """
-    dfCols = df.columns.to_list()
-    for col in dfCols:
-        print(f"Column: {col} \n{df[col].describe()} \nData Type: {df[col].dtype}\n")
