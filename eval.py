3d585f9a395c29e147615163100be0fd537fc4c6
Denis Reddig <d.reddig@uni-muenster.de>
2025-02-23 14:11:26 +0100

    Added docStrings and required params for all funcs

--- eval.py
+++ eval.py
@@ -1,8 +1,19 @@
 from sklearn import metrics
 import matplotlib.pyplot as pyplot
 
-def printConfusionMatrix(): # Confusion Matrix
-    metrics.ConfusionMatrixDisplay.from_estimator(final, xtest, ytest, cmap='Blues')
+def printConfusionMatrix(model, xtest, ytest):
+    """
+    Plot the confusion matrix for the given model.
+
+    Parameters:
+    model (XGBClassifier): The model to plot the confusion matrix from.
+    xtest (array-like): The test data.
+    ytest (array-like): The test labels.
+
+    Returns:
+    None
+    """
+    metrics.ConfusionMatrixDisplay.from_estimator(model, xtest, ytest, cmap='Blues')
     pyplot.show()
 
 def plotLossCurves(model):          
@@ -26,17 +37,51 @@ def plotLossCurves(model):
     pyplot.legend()
     pyplot.show()
 
-def printClassReport():     # Classification Report
-    print(metrics.classification_report(ytest, yhat, digits = 3))
+def printClassReport(ytest, yhat, precision=4):
+    """
+    Print the classification report for the given test labels and predictions.
 
-def printMisc():            # Best Iter, Test Accuracy, Base Score, Probas,
-    # Misc
+    Parameters:
+    ytest (array-like): The true labels for the test data.
+    yhat (array-like): The predicted labels from the model.
+    precision (int, optional): The number of decimal places to include in the report. Default is 4.
+
+    Returns:
+    None
+    """
+
+    print(metrics.classification_report(ytest, yhat, digits = precision))
+
+def printMisc(model, bestIter, ytest, yhat):
+    """
+    Print miscellaneous evaluation metrics for the given model.
+    Not intended for use!
+
+    Parameters:
+    model (XGBClassifier): The trained model used for evaluation.
+    bestIter (int): The best iteration or number of trees determined during training.
+    ytest (array-like): The true labels for the test data.
+    yhat (array-like): The predicted labels from the model.
+
+    Returns:
+    None
+    """
+
     print(f'# Trees / Best Iteration: \t{bestIter}')
-    print(f'Test Accuracy: \t{accuracy_score(ytest, yhat)}')
-    print(f'Base_Score{final.base_score}')
-    print(f'\nPredict_Proba Return: \n{final.predict_proba(xtest)}')
+    print(f'Test Accuracy: \t{metrics.accuracy_score(ytest, yhat)}')
+    print(f'Base_Score{model.base_score}')
+    print(f'\nPredict_Proba Return: \n{model.predict_proba(xtest)}')
 
-def printSummaryStatistics(df):   # Prints statistical for each column in the dataframe
+def printSummaryStatistics(df):
+    """
+    Prints the statistical summary for each column in the given dataframe.
+    
+    Parameters:
+    df (pandas.DataFrame): The dataframe to generate the summary statistics for.
+    
+    Returns:
+    None
+    """
     dfCols = df.columns.to_list()
     for col in dfCols:
         print(f"Column: {col} \n{df[col].describe()} \nData Type: {df[col].dtype}\n")
