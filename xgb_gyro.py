16d3fcb1ed16dd1d04a49a33636d9302d74ef459
Denis Reddig <d.reddig@uni-muenster.de>
2025-02-25 15:21:16 +0100

    Commented out func-calls

--- xgb_gyro.py
+++ xgb_gyro.py
@@ -73,8 +73,13 @@ trainModel(final, bestIter)
 
 yhat = final.predict(xtest)
 
-noClasses = final.n_classes_
-testList = st.genArrayList(xtest, 10)
-st.sendList(testList, noClasses, datasetsPath)
-ct.generateBaseCapture(final, xtest, datasetsPath)
-ct.generateComparison(datasetsPath)
+# noClasses = final.n_classes_
+# testList = st.genArrayList(xtest, 10)
+
+# print(xtest.columns.values)
+
+# st.sendArray(testList[0])
+
+# st.sendList(testList, noClasses, datasetsPath)
+# ct.generateBaseCapture(final, xtest, datasetsPath)
+# ct.generateComparison(datasetsPath)
